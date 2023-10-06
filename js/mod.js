48a63b2d211b0d6b6c8ea70e36a95bb15fb116f8
luhcactus690 <lukeleclair13@gmail.com>
2023-10-06 12:52:04 -0400

    Update mod.js
    changed name to humanity tree
    author; Luhcactushunchojack

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Humanity Tree",
+	id: "luhcactushunchojack",
+	author: "luhcactus",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
